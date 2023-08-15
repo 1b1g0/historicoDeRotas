b893eaf83891152a695a026bdfe26862c45ef54a
1b1g0 <igorbrunossi2@gmail.com>
2023-08-15 08:21:14 -0300

    limpando um pouquinho o codigo e o log.

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 // separar em rotas feitas no dia
 
 const fs = require('fs');
-
-const path = './2023_AUGUST.json'
+const path = '../2023_AUGUST.json'
 
  /*  VO COLOCAR OS PONTOS DE ACESSO A INFOS INÚTEIS AQUI \/ 
         
@@ -26,11 +25,10 @@ fs.readFile(`${path}`,'utf8', (err, data) => {
     try {
         // carregar todo o json
         const json = JSON.parse(data);
-
         let seletorAct, seletorPlc;
+
         // começo no 0
         const indiceItem = 71;
-        
         const totalPares = json.timelineObjects.length /2;
 
         // gambis pra arrumar o índice e pegar os pares corretos.
@@ -41,41 +39,36 @@ fs.readFile(`${path}`,'utf8', (err, data) => {
             seletorAct = undefined;
             seletorPlc = undefined;
             console.log(`Valor máximo do indice = ${totalPares - 1}.`);
-        }  
-        else {
+        }  else {
             seletorAct = indiceItem;
             seletorPlc = indiceItem + 1;
             console.log('eh zero')
         }
-
-        console.log('indices:',seletorAct,seletorPlc,'\n');
-
-        // json.timelineObjects[0].activitySegment.waypointPath -> pontos lat e lng, distancia e modo de viagem.
-        // BEM ÚTIL
-
+        //console.log('indices:',seletorAct,seletorPlc,'\n');
+        /* 
+        json.timelineObjects[0].activitySegment.waypointPath 
+        -> pontos lat e lng, distancia e modo de viagem. 
+        */
         const pontosRota = json.timelineObjects[seletorAct].activitySegment.waypointPath.waypoints;
         const distancia = json.timelineObjects[seletorAct].activitySegment.waypointPath.distanceMeters.toFixed(2);
         const l = json.timelineObjects[seletorPlc].placeVisit.location
-        
-        // const inicioFim = json.timelineObjects[seletorAct].activitySegment.duration;
 
-        // log HORRÍVEL pra comparar se a seleção está certa e pra explorar um poucos esses dados.
-        console.log('Coordenada final activitySegment: ',
-            pontosRota[pontosRota.length - 1],
-            '\nCoordenada inicial placeVisit:',
+        /* log HORRÍVEL pra comparar se a seleção está
+        certa e pra explorar um poucos esses dados. */
+        console.log('\nCoordenada final activitySegment:\n',
+            pontosRota[pontosRota.length - 1].latE7,
+            pontosRota[pontosRota.length - 1].lngE7,
+            '\nCoordenada inicial placeVisit:\n',
             l.latitudeE7,
             l.longitudeE7,
             `\nDist. Total: ${distancia}\n`,
-            '\n',
-            `\n Dif entre ultimo waypoint e primeiro place:\n`,
-            l.latitudeE7 - pontosRota[pontosRota.length - 1].latE7,' lat',
+            
+            `\nDif entre ultimo waypoint e primeiro place:\n`,
+            l.latitudeE7 - pontosRota[pontosRota.length - 1].latE7,'lat',
             l.longitudeE7 - pontosRota[pontosRota.length - 1].lngE7, 'lng\n',
             `\nInício da rota: ${json.timelineObjects[seletorAct].activitySegment.duration.startTimestamp.slice(11,19)}`,
             `\nFim da rota: ${json.timelineObjects[seletorAct].activitySegment.duration.endTimestamp.slice(11,19)}`,
-
             '\n\n----- fim -----');
-
-
     } catch (error){
         console.error('Erro lendo JSON', error);
     }
